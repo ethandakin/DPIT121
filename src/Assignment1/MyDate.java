d3d64054c884b5990f2205e7b6c669129b7a003f
Ethan <ethandakin@outlook.com.au>
2023-11-28 17:50:48 +1100

    :pig:

---
+++ src/Assignment1/MyDate.java
@@ -0,0 +1,67 @@
+package Assignment1;
+
+// Ethan Dakin
+// 8209194
+
+public class MyDate {
+    // Protected attributes for date
+    protected int year;
+    protected int month;
+    protected int day;
+
+    // Constructor
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    // Accessors
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    // Mutators
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.month = day;
+    }
+
+    public boolean isExpired(MyDate date) {
+        if (getYear() < date.getYear()) {
+            return true;
+        } else if (getYear() <= date.getYear() && getMonth() < date.getMonth()) {
+            return true;
+        } else if (getYear() <= date.getYear() && getMonth() <= date.getMonth() && getDay() <= date.getDay()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return String.format("%d/%d/%d", day, month, year);
+    }
+    
+    // Print method
+    public void print() {
+        System.out.printf("%d/%d/%d", day, month, year);
+    }
+}
---
+++ src/Assignment1/ThirdPartyPolicy.java
@@ -0,0 +1,34 @@
+package Assignment1;
+
+// Ethan Dakin
+// 8209194
+
+public class ThirdPartyPolicy extends InsurancePolicy{
+    // Attributes
+    protected String comments;
+
+    // Constructor
+    public ThirdPartyPolicy(String policyHolderName, int id, Car car, int numberOfClaims, MyDate expiryDate, String comments) {
+        super(policyHolderName, id, car, numberOfClaims, expiryDate);
+        this.comments = comments;
+    }
+
+    // calcPayment method, returns the premium payment for this class
+    @Override
+    public double calcPayment(double flatRate) {
+        return super.car.price / 100 + numberOfClaims * 200 + flatRate;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return String.format("%s\nComments: %s\n", super.toString(), this.comments);
+    }
+
+    // Print method
+    @Override
+    public void print() {
+        super.print();
+        System.out.printf("\nComments: %s\n", this.comments);
+    }
+}
