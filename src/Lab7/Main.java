19d92236a2e9c683e0052f5feab3a27460875b5f
Ethan <ethandakin@outlook.com.au>
2024-01-17 04:25:10 +1100

    nlehh

---
+++ src/Lab7/Main.java
@@ -0,0 +1,14 @@
+package Lab7;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Test");
+
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }  
+}
